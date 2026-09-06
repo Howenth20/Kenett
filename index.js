22c74af1053d928545da7e4152ebc492a9794fcc
Howenth20 <61111671@continental.edu.pe>
2026-09-06 10:57:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-API_KEY = "930b56c7e1ce4dcd99f153915260609";
+const API_KEY = `930b56c7e1ce4dcd99f153915260609`;
+const API = `https://api.weatherapi.com/v1/current.json?`;
+
+const response = await fetch(API);
+let data = response.json;
+
+let input = documentGetbyId("search");
+
+const getdata = () => {
+  let a = consolog(data);
+};
